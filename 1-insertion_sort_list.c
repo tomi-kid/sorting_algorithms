786372ef7812c6dd9b0bd999de59ff80259c0ea6
tomi-kid <tomiketema209@gmail.com>
2023-04-25 23:02:35 +0300

    updation of insertion sort

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -4,12 +4,13 @@
  * insertion_sort_list - sorts by insertion sort
  * @list: input list to sort
  * @pointer: previous node
- * @next: next node
+ * @nextt: next node
+ * @temp: used to store temporary node
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *temp, *pointer, *next;
+	listint_t *temp, *pointer, *nextt;
 
 	if (list && *list && (*list)->next)
 	{
@@ -21,7 +22,7 @@ void insertion_sort_list(listint_t **list)
 		{
 			if (temp->n < pointer->n)
 			{
-				swap_list(pointer, temp, nextt, list);
+				Swap_Linked_List(pointer, temp, nextt, list);
 			}
 			temp = nextt;
 			if (temp)
@@ -34,13 +35,13 @@ void insertion_sort_list(listint_t **list)
 }
 
 /**
- * swap_list - swap a linked list
+ * Swap_Linked_List - swap a linked list
  * @pn: previous node
  * @temp: temp
  * @nxt: next node
  * @h: head of list
  */
-void swap_list(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h)
+void Swap_Linked_List(listint_t *pn, listint_t *temp, listint_t *nxt, listint_t **h)
 {
 
 	while (pn && temp->n < pn->n)
