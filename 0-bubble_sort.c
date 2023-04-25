1a3a791a6da289966cbbb969814bcc242bae14fd
tomi-kid <tomiketema209@gmail.com>
2023-04-25 11:33:22 -0700

    sorting by bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -13,30 +13,29 @@
 
 void bubble_sort(int *array, size_t size)
 {
-    size_t c, j;
-    int temp;
+	size_t c, j;
+	int temp;
+	bool swap;
 
-    bool swap;
+	if (array == NULL)
+		return;
 
-    if (array == NULL)
-        return;
+	for (c = 0; c < size; c++)
+	{
+		swap = false;
 
-    for (c = 0; c < size; c++)
-    {
-        swap = false;
-
-        for (j = 0; j < size - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
-                temp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = temp;
-                print_array(array, size);
-                swap = true;
-            }
-        }
-        if (swap == false)
-            break;
-    }
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
+				swap = true;
+			}
+		}
+		if (swap == false)
+			break;
+	}
 }
